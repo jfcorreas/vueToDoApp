39905dd7d04da1d51144fd55d35a52fabb9ccb0b
Juanfran Correas <jfcorreas@gmail.com>
2017-10-09 14:39:50 +0200

    Component TodoList: todo data added

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <todo-list></todo-list>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -12,6 +12,27 @@ export default {
   name: 'app',
   components: {
     TodoList
+  },
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }]
+    }
   }
 }
 </script>
--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -9,7 +9,9 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: ['todos']
+  }
 </script>
 <style>
 </style>
