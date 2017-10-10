a27f0955f7d18c537252757376bf692b81b8823a
Juanfran Correas <jfcorreas@gmail.com>
2017-10-10 14:41:40 +0200

    Delete Todo implemented

--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -2,7 +2,10 @@
   <div>
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <todo  v-for="todo in todos" :key="todo.Title" v-bind:todo="todo"></todo>
+    <todo  v-for="todo in todos"
+          :key="todo.Title"
+          v-bind:todo="todo"
+          v-on:delete-todo="deleteTodo"></todo>
   </div>
 </template>
 
@@ -13,6 +16,12 @@
     props: ['todos'],
     components: {
       Todo
+    },
+    methods: {
+      deleteTodo (todo) {
+        const todoIndex = this.todos.indexOf(todo)
+        this.todos.splice(todoIndex, 1)
+      }
     }
   }
 </script>
